fa6b0e6298f0a014daad70fa7fe8886e386d9c7d
Jeremy Zongker <jeremy@trilitech.com>
2022-06-20 08:16:56 -0500

    Fixed compiler warning.

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,17 @@ import { Routing } from "./Routing";
 import { UserProvider } from "./UserContext";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { Themes } from "./appBase/helpers";
-const App: React.FC = () => {
-  return (<UserProvider>
-    <CookiesProvider>
-      <ThemeProvider theme={Themes.BaseTheme}>
-        <CssBaseline />
-        <Router>
-          <Routing />
-        </Router>
-      </ThemeProvider>
-    </CookiesProvider>
-  </UserProvider>);
-}
+
+const App: React.FC = () => (<UserProvider>
+  <CookiesProvider>
+    <ThemeProvider theme={Themes.BaseTheme}>
+      <CssBaseline />
+      <Router>
+        <Routing />
+      </Router>
+    </ThemeProvider>
+  </CookiesProvider>
+</UserProvider>);
+
 export default App;
 
